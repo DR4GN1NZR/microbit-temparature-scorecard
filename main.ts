03889909041484b099d9a215b3a392d14168cd28
DR4GN1NZR <84656811+DR4GN1NZR@users.noreply.github.com>
2021-05-23 08:51:49 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,20 @@
+let tmp = 0
+input.onButtonPressed(Button.A, function () {
+    tmp += -1
+    if (tmp < 18) {
+        basic.showString("C")
+    } else {
+        basic.showString("H")
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    tmp += 1
+    if (tmp < 18) {
+        basic.showString("C")
+    } else {
+        basic.showString("H")
+    }
+})
 basic.forever(function () {
 	
 })
